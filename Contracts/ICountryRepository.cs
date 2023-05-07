f6b733d40d848957b710cdd634765bfeca8a1710
git <luis.f.blanco@gmail.com>
2023-05-07 07:43:16 -0500

    End of Section 6, Video 36, Implement Repository Pattern - Part 1

---
+++ Contracts/ICountryRepository.cs
@@ -0,0 +1,8 @@
+using HotelListing.Data;
+
+namespace HotelListing.Contracts
+{
+    public interface ICountryRepository : IGenericRepository<Country> {
+
+    }
+}
---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using HotelListing.Contracts;
+using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly HotelListingDbContext _context;
+
+        public GenericRepository(HotelListingDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetAsync(id);
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int? id)
+        {
+            if (id is null)
+            {
+                return null;
+            }
+
+            return await _context.Set<T>().FindAsync(id);            
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
